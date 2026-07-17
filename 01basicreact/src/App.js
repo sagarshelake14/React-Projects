02ca86316c05c076b4e5c888fbcd6123e746ccdf
sagarshelake14 <sagarshelake0014@gmail.com>
2026-07-17 11:06:19 +0530

    creating basic react app using vite and CRA

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+import Chai from "./Chai";
+function App() {
+  return (
+    <>
+      <Chai />
+      <h1>Code Mith Me | Sagar Shelake</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+import Chai from "./Chai"
+function App() {
+  return (
+    <>
+      <Chai />
+      <h1>chai aur react</h1>
+    </>  // fragment
+  )
+}
+
+export default App
