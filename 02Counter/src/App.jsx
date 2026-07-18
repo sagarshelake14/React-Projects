f6bdf77d6bbd6710396eed41511739198f92cb6b
sagarshelake14 <sagarshelake0014@gmail.com>
2026-07-18 19:37:18 +0530

    simple counter project

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+function App() {
+  
+  let [counter, setCounter] = useState(15);
+  //let counter = 15;
+  
+  const addValue = () => {
+    if(counter < 20) counter = counter + 1;
+    setCounter(counter)
+  }
+
+  const removeValue = () => {
+    if (counter != 0 ) counter = counter - 1
+    setCounter(counter)
+  }
+
+  return (
+    <>
+      <h1>Code With Me</h1>
+      <h2>Counter value : {counter}</h2>
+
+      <button onClick={addValue} >Add Value {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove Value {counter}</button>
+      <p>footer : {counter}</p>
+    </>
+  )
+}
+
+export default App
